2172043f96f948ac334a060e2537b3f0099b2f79
fifty56 <fifty56@gmail.com>
2024-07-20 22:18:16 +0200

    upate

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "xFreezing", "xBracing", "xChilly", "xCool", "xMild", "xWarm", "xBalmy", "xHot", "xSweltering", "xScorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
